2b6289aff22714e2039cc8a87913990553fa8c96
Chintan Parikh <chintan@dynamicsats.onmicrosoft.com>
2026-02-25 14:07:33 +0530

    max file size limit to 50 mb

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ builder.Services.Configure<FormOptions>(options =>
 {
     options.MultipartBodyLengthLimit = 50 * 1024 * 1024; // 50MB
 });
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.MaxRequestBodySize = 50 * 1024 * 1024; // 50 MB
+});
 var app = builder.Build();
 
 //if (!app.Environment.IsDevelopment())
@@ -19,6 +23,31 @@ var app = builder.Build();
     app.UseHsts();
 //}
 
+app.Use(async (context, next) =>
+{
+    if (context.Request.ContentLength.HasValue)
+    {
+        const long maxBytes = 50L * 1024 * 1024; // 50 MB
+
+        if (context.Request.ContentLength.Value > maxBytes)
+        {
+            context.Response.StatusCode = StatusCodes.Status413PayloadTooLarge;
+            context.Response.ContentType = "text/html";
+
+            await context.Response.WriteAsync(@"
+                <html>
+                    <body style='font-family:Arial; text-align:center; margin-top:50px;'>
+                        <h1>File Too Large</h1>
+                        <p>The uploaded file exceeds the maximum allowed size.</p>
+                        <a href='/'>Go back</a>
+                    </body>
+                </html>
+            ");
+            return; 
+        }
+    }
+    await next();
+});
 app.UseHttpsRedirection();
 app.UseRouting();
 
