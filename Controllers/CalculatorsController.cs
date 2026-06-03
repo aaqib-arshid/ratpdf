095c0f6420a7cf8ca52c53e20177fcc8b1abc7cc
Aaqib Arshid <aaqib.arshid@outlook.com>
2026-06-03 12:18:20 +0530

    more tools

---
+++ Controllers/CalculatorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ratpdf.Controllers
+{
+    public class CalculatorsController : Controller
+    {
+        [HttpGet("bmi-calculator")] public IActionResult BMICalculator() => View();
+        [HttpGet("age-calculator")] public IActionResult AgeCalculator() => View();
+        [HttpGet("emi-calculator")] public IActionResult LoanCalculator() => View();
+        [HttpGet("percentage-calculator")] public IActionResult PercentageCalculator() => View();
+    }
+}
---
+++ Controllers/PdfToolsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ratpdf.Controllers
+{
+    public class PdfToolsController : Controller
+    {
+        [HttpGet("ocr-tool")] public IActionResult OCRTextExtractor() => View();
+        [HttpGet("pdf-redaction")] public IActionResult PdfRedaction() => View();
+    }
+}
